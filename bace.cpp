9271e5a2e677a9044a8c4b5d406171a7753a5f26
PoQue__ <farnfieldg@gmail.com>
2026-08-27 17:36:46 +0100

    Add files via upload

---
+++ bace.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+int main(){
+    
+    std::cout << "Press Enter to exit..." << '\n';
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+
+    return 0;
+}
